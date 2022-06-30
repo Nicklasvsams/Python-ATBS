68a80f3a7ed505edc77c05d56a2199711db24e37
nicklasvsams <nicklasvsams@gmail.com>
2022-06-30 09:56:47 +0200

    Restructured the project

--- snippets/coinflip.py
+++ snippets/coinflip.py
@@ -1,11 +1,13 @@
 from itertools import count
 import random
 numberOfStreaks = 0
+numberOfCoinflips = 0
 
 for experimentNumber in range(10000):
     # Code that creates a list of 100 'heads' or 'tails' values.
     coinFlipList = []
     for coinFlips in range (100):
+        numberOfCoinflips += 1
         if random.randint(0, 1) == 0:
             coinFlipList.append("H")
         else:
@@ -16,13 +18,11 @@ for experimentNumber in range(10000):
     # Code that checks if there is a streak of 6 heads or tails in a row.
     for i in range(len(coinFlipList)):
         currentCoin = coinFlipList[i]
-
-        if streak == 6:
+        if streak == 5:
             numberOfStreaks += 1
             streak = 0
-            lastCoin = ''
 
-        if currentCoin == lastCoin:
+        if currentCoin == "H" and currentCoin == lastCoin:
             streak += 1
         else:
             streak = 0
@@ -30,4 +30,6 @@ for experimentNumber in range(10000):
         lastCoin = coinFlipList[i]
     
 
-print('Chance of streak: %s%%' % (numberOfStreaks / (100 * 10000) * 100))
+print('Chance of streak: %s%%' % (numberOfStreaks / 10000))
+print(numberOfStreaks)
+print(numberOfCoinflips)
